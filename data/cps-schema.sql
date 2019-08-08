53d690df361b68d942e33ab25875f5bac936ce76
Kathleen Graham <kathleeng77@outlook.com>
2019-08-07 21:13:05 -0500

    completed schema and loading dataframes to database

--- data/cps-schema.sql
+++ data/cps-schema.sql
@@ -1,4 +1,10 @@
+DROP TABLE IF EXISTS school_profiles CASCADE;
+DROP TABLE IF EXISTS school_reports CASCADE;
+DROP TABLE IF EXISTS school_locations CASCADE;
+DROP TABLE IF EXISTS fast_food CASCADE;
+
 CREATE TABLE school_profiles (
+	index serial,
 	school_id INT PRIMARY KEY,
 	finance_id INT,
 	short_name VARCHAR(225),
@@ -8,14 +14,16 @@ CREATE TABLE school_profiles (
 );
 
 CREATE TABLE school_reports (
-	school_id INT PRIMARY KEY,
+	index serial,
+	school_id INT,
 	short_name VARCHAR(225),
-	award VARCHAR(255)
-	
+	award VARCHAR(255),
+	FOREIGN KEY (school_id) REFERENCES school_profiles (school_id)
 );
 
 CREATE TABLE school_locations (
-	school_id INT PRIMARY KEY,
+	index serial,
+	school_id INT,
 	network VARCHAR(225),
 	short_name VARCHAR(225),
 	address VARCHAR(225),
@@ -23,12 +31,17 @@ CREATE TABLE school_locations (
 	grades VARCHAR(225),
 	community VARCHAR(225),
 	ward INT,
-	alderman VARCHAR(225)	
+	alderman VARCHAR(225)
 );
 
 CREATE TABLE fast_food (
-	id INT PRIMARY KEY,
+	index serial,
 	name VARCHAR(225),
 	zip INT,
-	province VARCHAR(225)
+	state VARCHAR(225)
 );
+
+select * from school_profiles;
+select * from school_reports;
+select * from school_locations;
+select * from fast_food;
